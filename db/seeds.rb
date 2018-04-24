81c4cd06cda96e1cf15c2d83cfd4be5cfd6dd686
Gauthier Masse <masse.gauthier@gmail.com>
2018-04-24 17:15:39 +0200

    commit 17h15

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,10 @@ end
 
 #on crée des articles random
 100.times do
-  article = Article.create(name: Faker::Company.name, body: Faker::Company.catch_phrase, description: Faker::Company.catch_phrase)
+  article = Article.create(name: Faker::Company.name, body: Faker::Company.catch_phrase, description: Faker::Company.catch_phrase, user_id:Faker::Number.between(1, 100), category_id: Faker::Number.between(1, 200))
+end
+
+#on crée des catégories random
+200.times do
+	category = Category.create(name: Faker::Beer.name)
 end
